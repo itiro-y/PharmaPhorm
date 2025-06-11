8335496da9af65015cad279e709dd32102c341b2
matheuskya <kenzoyamashitaasano@gmail.com>
2025-06-11 09:48:51 -0300

    Projeto criado

---
+++ src/main/java/com/example/PharmaPhorm/PharmaPhormApplication.java
@@ -0,0 +1,13 @@
+package com.example.PharmaPhorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaPhormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaPhormApplication.class, args);
+	}
+
+}
